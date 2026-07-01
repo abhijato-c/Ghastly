9cf764eeb58ccceb23dc9dbfa9e29a0ceb7033d5
abhijato-c <abhijato.c@gmail.com>
2026-07-01 19:18:28 +0530

    Added level selector

--- TitleScreen/MenuController.gd
+++ TitleScreen/MenuController.gd
@@ -13,57 +13,87 @@ extends Node
 @export var SelectedEmbolden: float = 0.5
 @export var SelectedSpacing: float = 0
 
+@export_group("Menu Objects")
+@export var MenuObjects: Array[Panel]
+@export var LevelHighlight: TextureRect
+@export var LevelGrid: GridContainer
 
-var SelIndex: int = 0
-var OptionLabels: Array[Node] = []
+var MenuOpened: int = 0
+var MenuIndex: int = 0
+var LevelIndex: int = 0
+var MenuLabels: Array[Node] = []
+var LevelLabels: Array[Node] = []
 
 func _ready() -> void:
-	OptionLabels = MenuOptions.get_children()
-	UpdateSelection()
+	MenuLabels = MenuOptions.get_children()
+	UpdateMenuSelection()
+	
+	LevelLabels = LevelGrid.get_children()
+	UpdateLevelSelection()
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_up") and SelIndex > 0:
-		SelIndex -= 1
-		UpdateSelection()
-	elif event.is_action_pressed("ui_down") and SelIndex < OptionLabels.size() - 1:
-		SelIndex += 1
-		UpdateSelection()
+	if event.is_action_pressed("ui_cancel"):
+		for panel: Panel in MenuObjects:
+			panel.hide()
+		MenuOpened = 0
+	elif MenuOpened == 0:
+		MenuAction(event)
+	elif MenuOpened == 1:
+		LevelAction(event)
+
+func MenuAction(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_up") and MenuIndex > 0:
+		MenuIndex -= 1
+		UpdateMenuSelection()
+	elif event.is_action_pressed("ui_down") and MenuIndex < MenuLabels.size() - 1:
+		MenuIndex += 1
+		UpdateMenuSelection()
 	elif event.is_action_pressed("ui_accept"):
-		TriggerSelection()
+		MenuObjects[MenuIndex].show()
+		MenuOpened = MenuIndex + 1
 
-func UpdateSelection() -> void:
-	for i in range(OptionLabels.size()):
-		var label = OptionLabels[i] as Label
-		if i == SelIndex:
-			SelectOption(label);
+func LevelAction(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_up") and LevelIndex > 4:
+		LevelIndex -= 5
+	elif event.is_action_pressed("ui_down") and LevelIndex < 5:
+		LevelIndex += 5
+	elif  event.is_action_pressed("ui_left") and LevelIndex > 0:
+		LevelIndex -= 1
+	elif  event.is_action_pressed("ui_right") and LevelIndex < 9:
+		LevelIndex += 1
+	UpdateLevelSelection()
+
+func UpdateMenuSelection() -> void:
+	for i in range(MenuLabels.size()):
+		var option = MenuLabels[i] as Label
+		if i == MenuIndex:
+			option.add_theme_font_size_override("font_size", SelectedFontSize)
+			option.add_theme_color_override("font_color", SelectedColor)
+			option.size_flags_stretch_ratio = SelectedStretch;
+			
+			var Width = MenuOptions.size.x
+			var Style = StyleBoxEmpty.new()
+			Style.content_margin_left = Width * SelectedIndent
+			option.add_theme_stylebox_override("normal", Style)
+			
+			var Variation = FontVariation.new()
+			Variation.base_font = BaseFont
+			Variation.variation_embolden = SelectedEmbolden
+			Variation.spacing_glyph = SelectedSpacing
+			option.add_theme_font_override("font", Variation)
 		else:
-			DeselectOption(label);
+			option.add_theme_font_size_override("font_size", DefaultFontSize)
+			option.add_theme_color_override("font_color", DefaultColor)
+			option.size_flags_stretch_ratio = DefaultStretch;
+			option.add_theme_font_override("font", BaseFont)
+			
 
-func TriggerSelection() -> void:
-	print(SelIndex)
 
-func SelectOption(option: Label) -> void:
-	option.add_theme_font_size_override("font_size", SelectedFontSize)
-	option.add_theme_color_override("font_color", SelectedColor)
-	option.size_flags_stretch_ratio = SelectedStretch;
-	
-	var Width = MenuOptions.size.x
-	var Style = StyleBoxEmpty.new()
-	Style.content_margin_left = Width * SelectedIndent
-	option.add_theme_stylebox_override("normal", Style)
-	
-	var Variation = FontVariation.new()
-	Variation.base_font = BaseFont
-	Variation.variation_embolden = SelectedEmbolden
-	Variation.spacing_glyph = SelectedSpacing
-	option.add_theme_font_override("font", Variation)
+			var Style = StyleBoxEmpty.new()
+			Style.content_margin_left = 0
+			option.add_theme_stylebox_override("normal", Style)
 
-func DeselectOption(option: Label) -> void:
-	option.add_theme_font_size_override("font_size", DefaultFontSize)
-	option.add_theme_color_override("font_color", DefaultColor)
-	option.size_flags_stretch_ratio = DefaultStretch;
-	option.add_theme_font_override("font", BaseFont)
-	
-	var Style = StyleBoxEmpty.new()
-	Style.content_margin_left = 0
-	option.add_theme_stylebox_override("normal", Style)
+func UpdateLevelSelection() -> void:
+	var LevelLabel = LevelLabels[LevelIndex] as Control
+	var offset = (LevelLabel.size - LevelHighlight.size) / 2
+	LevelHighlight.global_position = LevelLabel.global_position + offset
