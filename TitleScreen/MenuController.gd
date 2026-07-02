6ac0c1563c2a6b5bbba5bfa643b4af8539bc9253
abhijato-c <abhijato.c@gmail.com>
2026-07-02 19:30:06 +0530

    Finish basic functioning for title screen

--- TitleScreen/MenuController.gd
+++ TitleScreen/MenuController.gd
@@ -17,19 +17,48 @@ extends Node
 @export var MenuObjects: Array[Panel]
 @export var LevelHighlight: TextureRect
 @export var LevelGrid: GridContainer
+@export var TutorialLabel: Label
+@export var TutorialTexts: Array[String]
+
+@export_group("Textures")
+@export var LockIcon: Texture2D
 
 var MenuOpened: int = 0
 var MenuIndex: int = 0
 var LevelIndex: int = 0
+var TutorialIndex: int = 0
 var MenuLabels: Array[Node] = []
 var LevelLabels: Array[Node] = []
+var UnlockedLevel: int = 0
+const SavePath: String = "user://Save.json"
 
 func _ready() -> void:
+	if not FileAccess.file_exists(SavePath):
+		var file: FileAccess = FileAccess.open(SavePath, FileAccess.WRITE)
+		var json: Dictionary = {"UnlockedLevel": 0}
+		file.store_string(JSON.stringify(json))
+		file.close()
+		UnlockedLevel = 0
+	else:
+		print("existing")
+		var file: FileAccess = FileAccess.open(SavePath, FileAccess.READ)
+		var json = JSON.new()
+		var _res = json.parse(file.get_as_text())
+		var data: Dictionary = json.get_data() as Dictionary
+		UnlockedLevel = data["UnlockedLevel"]
+	
 	MenuLabels = MenuOptions.get_children()
 	UpdateMenuSelection()
 	
 	LevelLabels = LevelGrid.get_children()
 	UpdateLevelSelection()
+	for i in range(UnlockedLevel + 1, 10):
+		var label: Label = LevelLabels[i]
+		var Style = label.get_theme_stylebox("normal").duplicate() as StyleBoxTexture
+		Style.texture = LockIcon
+		label.add_theme_stylebox_override("normal", Style)
+	
+	UpdateTutorialSelection()
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -40,6 +69,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		MenuAction(event)
 	elif MenuOpened == 1:
 		LevelAction(event)
+	elif MenuOpened == 2:
+		TutorialAction(event)
 
 func MenuAction(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_up") and MenuIndex > 0:
@@ -49,19 +80,37 @@ func MenuAction(event: InputEvent) -> void:
 		MenuIndex += 1
 		UpdateMenuSelection()
 	elif event.is_action_pressed("ui_accept"):
+		if MenuIndex == 3:
+			get_tree().quit()
+			return
 		MenuObjects[MenuIndex].show()
 		MenuOpened = MenuIndex + 1
 
 func LevelAction(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_up") and LevelIndex > 4:
+	var PrevLevel = LevelIndex
+	if event.is_action_pressed("ui_up"):
 		LevelIndex -= 5
-	elif event.is_action_pressed("ui_down") and LevelIndex < 5:
+	elif event.is_action_pressed("ui_down"):
 		LevelIndex += 5
-	elif  event.is_action_pressed("ui_left") and LevelIndex > 0:
+	elif  event.is_action_pressed("ui_left"):
 		LevelIndex -= 1
-	elif  event.is_action_pressed("ui_right") and LevelIndex < 9:
+	elif  event.is_action_pressed("ui_right"):
 		LevelIndex += 1
-	UpdateLevelSelection()
+	elif event.is_action_pressed("ui_accept") and LevelIndex <= UnlockedLevel:
+		get_tree().change_scene_to_file("res://Levels/Level-{n}/Level-{n}.tscn".format({"n": LevelIndex + 1}))
+		return
+	
+	if LevelIndex < 0 or LevelIndex > 9 or LevelIndex > UnlockedLevel:
+		LevelIndex = PrevLevel
+	else:
+		UpdateLevelSelection()
+
+func TutorialAction(event: InputEvent) -> void:
+	if  event.is_action_pressed("ui_left") and TutorialIndex > 0:
+		TutorialIndex -= 1
+	elif  event.is_action_pressed("ui_right") and TutorialIndex < TutorialTexts.size() - 1:
+		TutorialIndex += 1
+	UpdateTutorialSelection()
 
 func UpdateMenuSelection() -> void:
 	for i in range(MenuLabels.size()):
@@ -97,3 +146,6 @@ func UpdateLevelSelection() -> void:
 	var LevelLabel = LevelLabels[LevelIndex] as Control
 	var offset = (LevelLabel.size - LevelHighlight.size) / 2
 	LevelHighlight.global_position = LevelLabel.global_position + offset
+
+func UpdateTutorialSelection() -> void:
+	TutorialLabel.set_text(TutorialTexts[TutorialIndex].replacen("\\n", "\n"))
